591e5be737f7d5306ac5b6e9f305335eb1e5385c
Hamza Shehzad <Hamzashehzad59@gmail.com>
2025-10-18 14:07:26 +0000

    Try fixing this error: `Console Error: In HTML, <a> cannot be a descenda

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -26,14 +26,14 @@ export function Header({ isConnected, onConnectClick }: HeaderProps) {
         <NavigationMenu>
             <NavigationMenuList>
                 <NavigationMenuItem>
-                    <Link href="/" passHref>
+                    <Link href="/" legacyBehavior passHref>
                         <NavigationMenuLink className={navigationMenuTriggerStyle()}>
                             Metadata Generator
                         </NavigationMenuLink>
                     </Link>
                 </NavigationMenuItem>
                 <NavigationMenuItem>
-                    <Link href="/wp-alt-text" passHref>
+                    <Link href="/wp-alt-text" legacyBehavior passHref>
                         <NavigationMenuLink className={navigationMenuTriggerStyle()}>
                             WP Alt Text
                         </NavigationMenuLink>
