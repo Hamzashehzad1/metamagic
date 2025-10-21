3f6443c5d3cc7e9e41a25e819e5a8e239b4fec22
Hamza Shehzad <Hamzashehzad59@gmail.com>
2025-10-21 00:31:43 +0000

    Set a strict Content-Security-Policy response header across all page to

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,21 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   async headers() {
+    const cspHeader = `
+      default-src 'self';
+      script-src 'self' 'unsafe-eval' 'unsafe-inline' *.googletagmanager.com;
+      style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
+      img-src 'self' data: https://placehold.co https:;
+      font-src 'self' https://fonts.gstatic.com;
+      connect-src 'self' https://*.firebaseapp.com https://*.firebaseio.com https://*.googleapis.com wss:;
+      frame-src 'self' https://*.firebaseapp.com;
+      object-src 'none';
+      base-uri 'self';
+      form-action 'self';
+      frame-ancestors 'none';
+      upgrade-insecure-requests;
+    `.replace(/\s{2,}/g, ' ').trim();
+
     return [
       {
         source: '/:path*',
@@ -18,6 +33,10 @@ const nextConfig: NextConfig = {
           {
             key: 'X-Content-Type-Options',
             value: 'nosniff',
+          },
+          {
+            key: 'Content-Security-Policy',
+            value: cspHeader,
           }
         ],
       },
