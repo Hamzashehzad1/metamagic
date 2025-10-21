1d6d6a3f111897d7c576cdf3fcabd0215fd493ad
Hamza Shehzad <Hamzashehzad59@gmail.com>
2025-10-21 00:28:16 +0000

    To minimise security issues, the X-Content-Type-Options response header

--- next.config.ts
+++ next.config.ts
@@ -14,6 +14,10 @@ const nextConfig: NextConfig = {
           {
             key: 'X-Frame-Options',
             value: 'SAMEORIGIN',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
           }
         ],
       },
