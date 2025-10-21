d2b6ba8c3d6b6794660b591ed4a6b6c8b512dbfe
Hamza Shehzad <Hamzashehzad59@gmail.com>
2025-10-21 01:39:13 +0000

    Minify JavaScript Est savings of 24 KiB Minifying JavaScript files can r

--- next.config.mjs
+++ next.config.mjs
@@ -1,74 +1,28 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async headers() {
-    const cspHeader = `
-      default-src 'self';
-      script-src 'self' 'unsafe-eval' 'unsafe-inline' *.googletagmanager.com;
-      style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
-      img-src 'self' data: blob: https://placehold.co https://*;
-      font-src 'self' https://fonts.gstatic.com;
-      connect-src 'self' https://*.firebaseapp.com https://*.firebaseio.com https://*.googleapis.com * ws://* wss://*;
-      frame-src 'self' https://*.firebaseapp.com *;
-      object-src 'none';
-      base-uri 'self';
-      form-action 'self';
-      frame-ancestors 'none';
-      upgrade-insecure-requests;
-    `.replace(/\s{2,}/g, ' ').trim();
-
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+      },
+    ],
+  },
+  headers: async () => {
     return [
       {
         source: '/:path*',
         headers: [
           {
-            key: 'Referrer-Policy',
-            value: 'strict-origin-when-cross-origin',
-          },
-          {
-            key: 'X-Frame-Options',
-            value: 'SAMEORIGIN',
-          },
-          {
-            key: 'X-Content-Type-Options',
-            value: 'nosniff',
-          },
-          {
             key: 'Content-Security-Policy',
-            value: cspHeader,
-          }
+            value:
+              "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline' *.googletagmanager.com; style-src 'self' 'unsafe-inline'; img-src 'self' data: blob: https://placehold.co https://*; font-src 'self'; connect-src 'self' https://*.firebaseapp.com https://*.firebaseio.com https://*.googleapis.com * ws://* wss://*; frame-src 'self' https://*.firebaseapp.com *; object-src 'none'; base-uri 'self'; form-action 'self'; frame-ancestors 'none'; upgrade-insecure-requests;",
+          },
         ],
       },
     ];
-  },
-  experimental: {
-    serverActions: {
-      bodySizeLimit: '4.5mb',
-    },
-  },
-  /* config options here */
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'http',
-        hostname: '**',
-      },
-      {
-        protocol: 'https',
-        hostname: '**',
-      }
-    ],
   },
 };
 
